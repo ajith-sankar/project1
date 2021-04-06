2515db63931f502f2e137126255126ce728ab489
aju <ajithsankaras.007@gmail.com>
2021-04-06 09:49:28 +0530

    demo work

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  token: ''
+  token: 'ya29.c.Kp8B-gfty9velLL_AQ_r_qvxqrdtwrPct4gEDyKfMMQ5pTZqWCshBifuDXQ-1jP-JzYR20Nb84-jole3uBALLrHig4oxm_SBxMCEIy3H8M8T-rPu0679gXwEQLac74yiNEaJEPRIC4HdmIHB7uxS_WheyotQKl7eb2KNKzeVmy7EdxmDrc_--690d7Ldr3tjGyzGdytmsBwIl0vKXyze7_zo'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  token: ''
+  production: true,
+  token: 'ya29.c.Kp8B-gfty9velLL_AQ_r_qvxqrdtwrPct4gEDyKfMMQ5pTZqWCshBifuDXQ-1jP-JzYR20Nb84-jole3uBALLrHig4oxm_SBxMCEIy3H8M8T-rPu0679gXwEQLac74yiNEaJEPRIC4HdmIHB7uxS_WheyotQKl7eb2KNKzeVmy7EdxmDrc_--690d7Ldr3tjGyzGdytmsBwIl0vKXyze7_zo'
 };
